e1c20c3d273b11605e44a0027a0b6cf96490bd6c
Jack Hubert <jphubert93@gmail.com>
2021-08-19 17:06:03 -0600

    successfully accessed Spotify API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { Component } from 'react';
 import './App.css';
+import { authorization_request } from './secrets';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    console.log(authorization_request)
+    console.log(auth2)
+    fetch(`https://accounts.spotify.com/api/token`, {
+      method: 'POST',
+      headers: {
+        Authorization: authorization_request,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials'
+    })
+      .then( response => response.json() )
+      .then( result => console.log(result))
+  }
+
+  render(){
+    return (
+      <div className="App">
+      </div>
+    )
+  }
 }
 
 export default App;
