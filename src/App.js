947dc57e3c5236a7b5e622a9c3bb0fb7565f5d67
Jack Hubert <jphubert93@gmail.com>
2021-08-19 17:19:53 -0600

    set AuthToken as spotify_token in localStorage; created a function to get sample artists using a 'get artists' button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Component } from 'react';
 import './App.css';
 import { authorization_request } from './secrets';
 
+const parseHTTPResponse = response => response.json()
+
 class App extends Component {
 
   componentDidMount(){
-    console.log(authorization_request)
-    console.log(auth2)
     fetch(`https://accounts.spotify.com/api/token`, {
       method: 'POST',
       headers: {
@@ -15,13 +15,26 @@ class App extends Component {
       },
       body: 'grant_type=client_credentials'
     })
-      .then( response => response.json() )
-      .then( result => console.log(result))
+      .then( parseHTTPResponse )
+      .then( ({ access_token }) => {
+        localStorage.setItem('spotify_token', access_token)
+      })
+  }
+
+  getArtists = () => {
+    fetch('https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6', {
+      headers: {
+        Authorization: `Bearer ${localStorage.spotify_token}`
+      }
+    })
+      .then( parseHTTPResponse )
+      .then( console.log )
   }
 
   render(){
     return (
       <div className="App">
+        <button onClick={this.getArtists}>Get Artists</button>
       </div>
     )
   }
