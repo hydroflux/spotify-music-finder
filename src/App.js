e87740308663e9d953dace3739f947f33262dc93
Jack Hubert <jphubert93@gmail.com>
2021-08-19 17:34:56 -0600

    got rid of artist button & function, focusing on featured playlists for B&B; refactored token request into getSpotifyToken for component did mount & executed getFeaturedPlaylist on mount

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ const parseHTTPResponse = response => response.json()
 class App extends Component {
 
   componentDidMount(){
+    this.getSpotifyToken()
+  }
+
+  getSpotifyToken = () => {
     fetch(`https://accounts.spotify.com/api/token`, {
       method: 'POST',
       headers: {
@@ -18,23 +22,14 @@ class App extends Component {
       .then( parseHTTPResponse )
       .then( ({ access_token }) => {
         localStorage.setItem('spotify_token', access_token)
+        this.getFeaturedPlaylists( access_token )
       })
   }
 
-  getArtists = () => {
-    fetch('https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6', {
-      headers: {
-        Authorization: `Bearer ${localStorage.spotify_token}`
-      }
-    })
-      .then( parseHTTPResponse )
-      .then( console.log )
-  }
-
-  getPlaylists = () => {
+  getFeaturedPlaylists = spotify_token => {
     fetch('https://api.spotify.com/v1/browse/featured-playlists', {
       headers: {
-        Authorization: `Bearer ${localStorage.spotify_token}`,
+        Authorization: `Bearer ${spotify_token}`,
         Accept: 'application/json',
         'Content-Type': 'application/json'
       }
@@ -46,7 +41,6 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <button onClick={this.getArtists}>Get Artists</button>
         <button onClick={this.getPlaylists}>Get Playlists</button>
       </div>
     )
