a5fb3b41e87627bbea8c2ffcdbe5137346c91f34
Jack Hubert <jphubert93@gmail.com>
2021-08-19 17:53:59 -0600

    created a helpers folder & a utilities script in order to clean up the process of getting the spotify token & featured playlists

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Component } from 'react';
 import './App.css';
-import { authorization_request } from './secrets';
-
-const parseHTTPResponse = response => response.json()
+import { authFetch, authorization_request, parseHTTPResponse } from './helpers/utilities';
 
 class App extends Component {
 
@@ -11,29 +9,34 @@ class App extends Component {
   }
 
   getSpotifyToken = () => {
-    fetch(`https://accounts.spotify.com/api/token`, {
-      method: 'POST',
-      headers: {
-        Authorization: authorization_request,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials'
-    })
+    const headers = {
+      Authorization: authorization_request,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    authFetch(`https://accounts.spotify.com/api/token`,
+      'POST',
+      headers,
+      'grant_type=client_credentials')
       .then( parseHTTPResponse )
-      .then( ({ access_token }) => {
-        localStorage.setItem('spotify_token', access_token)
-        this.getFeaturedPlaylists( access_token )
-      })
+      .then( this.handleAccessToken )
+  }
+
+  handleAccessToken = ({ access_token }) => {
+    localStorage.setItem('spotify_token', access_token)
+    this.getFeaturedPlaylists( access_token )
   }
 
   getFeaturedPlaylists = spotify_token => {
-    fetch('https://api.spotify.com/v1/browse/featured-playlists', {
-      headers: {
-        Authorization: `Bearer ${spotify_token}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
+    const headers = {
+      Authorization: `Bearer ${spotify_token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+
+    authFetch('https://api.spotify.com/v1/browse/featured-playlists',
+      'GET',
+      headers)
       .then( parseHTTPResponse )
       .then( console.log )
   }
