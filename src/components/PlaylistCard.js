4217cc3a39d764d073973885a6aca7cff8dcaa0b
Jack Hubert <jphubert93@gmail.com>
2021-08-19 18:14:45 -0600

    updated PlaylistCard component in order to display name, description, & playlist images

--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function PlaylistCard() {
+export default function PlaylistCard({ playlist }) {
+    
+    const { name, description, images } = playlist
+
     return (
         <div className="playlist-card">
-            
+            <h2>{ name }</h2>
+            <p>{ description }</p>
+            <img
+                src={ images[0].url }
+                alt={ name }
+            />
         </div>
     )
 }
--- src/containers/PlaylistsContainer.js
+++ src/containers/PlaylistsContainer.js
@@ -8,7 +8,8 @@ export default function PlaylistsContainer({ playlists }) {
             return (
                 <PlaylistCard
                     key={playlist.id}
-                    {...playlist}/>
+                    playlist={playlist}
+                />
             )
         })   
     }
