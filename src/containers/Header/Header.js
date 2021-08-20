523d44e819fe686774fad97341d6f15a2b6b3115
Jack Hubert <jphubert93@gmail.com>
2021-08-19 19:24:04 -0600

    adjusted header styling & added an id; created skeleton tfor search bar

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,10 +1,16 @@
 import './Header.css'
 import SpotifyLogo from '../../assets/images/spotify-logo.png'
+import SearchBar from '../../components/SearchBar/SearchBar'
 
 export default function Header() {
     return (
         <header>
-            <img src={ SpotifyLogo } alt="logo"/>
+            <img
+                id="logo"
+                src={ SpotifyLogo }
+                alt="logo"
+            />
+            <SearchBar /> 
         </header>
     )
 }
