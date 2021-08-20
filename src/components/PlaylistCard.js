058b9c3be68995a81a13a760e9928987d4d7e1ce
Jack Hubert <jphubert93@gmail.com>
2021-08-19 18:03:56 -0600

    created folders for components & containers; set up skeleton structure for PlaylistsContainer & PlaylistsCard

---
+++ src/components/PlaylistCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PlaylistCard() {
+    return (
+        <div className="playlist-card">
+            
+        </div>
+    )
+}
---
+++ src/containers/PlaylistsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function PlaylistsContainer() {
+    return (
+        <section className="playlists-container" >
+        </section>
+    )
+}
